0e2330175edc8bd2f102c58123e45700bd041bee
Andrew <e1398281@u.nus.edu>
2025-08-17 10:35:17 +0800

    added witbot level 0

---
+++ WitBot/src/WitBot.java
@@ -0,0 +1,9 @@
+public class WitBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hey there human (I hope you are...)! I'm WitBot - your unconventionally helpful sidekick!");
+        System.out.println("What idea, plan or meme do you have on mind today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Awh... so fast? Alright then, hope to see you again soon!");
+    }
+}
