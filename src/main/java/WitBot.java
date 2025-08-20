e52942bce221c94f7d7b22b0acd1249f72a89ecf
Andrew <e1398281@u.nus.edu>
2025-08-20 22:21:22 +0800

    Add increment: Level 1 & empty input check

--- src/main/java/WitBot.java
+++ src/main/java/WitBot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class WitBot {
 
     public static void main(String[] args) {
@@ -12,7 +14,28 @@ public class WitBot {
         System.out.println("\tHey there human (I hope you are one...)! I'm WitBot - your unconventionally helpful sidekick!\n" + logo);
         System.out.println("\tWhat idea, plan or meme do you have on mind today?");
         System.out.println(tab);
+
+        while (true) {
+            //        echos user's input
+            Scanner sc = new Scanner(System.in);
+            String echo_word;
+            echo_word = sc.nextLine();
+
+            if (echo_word.equals("bye")) {
+                break;
+            } else if (echo_word.isEmpty()) {
+//                empty input check
+                System.out.println(tab);
+                System.out.println("Oops! You've gotta input something~");
+                System.out.println(tab);
+            } else {
+                System.out.println(tab);
+                System.out.println("\t" + echo_word);
+                System.out.println(tab);
+            }
+        }
         //        says bye to user
+        System.out.println(tab);
         System.out.println("\tAwh... so fast? Alright then, hope to see you again soon!");
         System.out.println(tab);
     }
