df967a45ae3e9c8f995cb6f990347a53c25edea2
Andrew <e1398281@u.nus.edu>
2025-08-20 22:11:27 +0800

    Add increment: Level 0

---
+++ src/main/java/WitBot.java
@@ -0,0 +1,19 @@
+public class WitBot {
+
+    public static void main(String[] args) {
+        //        greeting user
+        String tab = "\t____________________________________________________________";
+        System.out.println(tab);
+        String logo = "       __          ___   _      \n" +
+                "       \\ \\        / / | (_) |_   \n" +
+                "        \\ \\  /\\  / /  | | | __|  \n" +
+                "         \\ \\/  \\/ /   | | | |_   \n" +
+                "          \\__/\\__/    |_|  \\__|  \n";
+        System.out.println("\tHey there human (I hope you are one...)! I'm WitBot - your unconventionally helpful sidekick!\n" + logo);
+        System.out.println("\tWhat idea, plan or meme do you have on mind today?");
+        System.out.println(tab);
+        //        says bye to user
+        System.out.println("\tAwh... so fast? Alright then, hope to see you again soon!");
+        System.out.println(tab);
+    }
+}
