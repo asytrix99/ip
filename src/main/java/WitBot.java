1bba7a3dfcd6b71f85178d1ca8dc854391c39ca3
Andrew <e1398281@u.nus.edu>
2025-08-20 22:39:50 +0800

    Add increment: Level 2. Add, List & empty list check

--- src/main/java/WitBot.java
+++ src/main/java/WitBot.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class WitBot {
 
@@ -15,25 +15,41 @@ public class WitBot {
         System.out.println("\tWhat idea, plan or meme do you have on mind today?");
         System.out.println(tab);
 
-        while (true) {
+
             //        echos user's input
             Scanner sc = new Scanner(System.in);
             String echo_word;
-            echo_word = sc.nextLine();
+            ArrayList<String> todo_list = new ArrayList<>();
+            int task_index = 1;
 
+        while (true) {
+            echo_word = sc.nextLine();
             if (echo_word.equals("bye")) {
                 break;
             } else if (echo_word.isEmpty()) {
 //                empty input check
+                System.out.println("\tOops! You've gotta input something~");
                 System.out.println(tab);
-                System.out.println("Oops! You've gotta input something~");
+            } else if (echo_word.equals("list")) {
+                System.out.println(tab);
+                if (todo_list.isEmpty()) {
+                    System.out.println("\tYou're free for the day dear!");
+                    System.out.println(tab);
+                    continue;
+                }
+                for (String task : todo_list) {
+                    System.out.println("\t" + task_index + ". " + task);
+                    task_index++;
+                }
                 System.out.println(tab);
             } else {
+                todo_list.add(echo_word);
                 System.out.println(tab);
-                System.out.println("\t" + echo_word);
+                System.out.println("\tadded: " + echo_word);
                 System.out.println(tab);
             }
         }
+
         //        says bye to user
         System.out.println(tab);
         System.out.println("\tAwh... so fast? Alright then, hope to see you again soon!");
